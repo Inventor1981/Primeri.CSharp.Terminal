a593fd2d60c456a983a5f52f6c73dbb33577e55a
Владо Димитров <vlado198152@gmail.com>
2017-04-28 21:06:16 +0300

    Модул 02, Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+
+
+
+			//Примерен коментар
+
+			/*
+			 Ред 1
+			 Ред 2
+			 Ред 3
+			 */
+		}
+	}
+}
